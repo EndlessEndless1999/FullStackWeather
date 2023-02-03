1cb25729c8aca35059f506bb0a6bf3cd15863013
EndlessEndless1999 <115580045+EndlessEndless1999@users.noreply.github.com>
2023-02-03 15:19:16 +0000

    adds further functions + temp object

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,17 @@ let longitude = '-0.12120';
 const APIkey = '70126ef86ab9fc4f2eea754dbbaf84a3';
 const queryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + APIkey;
 
+let day;
+
+let tempEvals = {
+    hot: 30,
+    mild: 20,
+    cold: 10
+}
+
+// let searchedCities = {};
+
+
 
 function exampleCall(){
     $.ajax({
@@ -14,5 +25,16 @@ function exampleCall(){
         console.log(response);
     })
 }
+
+function getTime(){
+    day = moment();
+    console.log(day._d);
+}
+
+function setWeatherIcon(){
+
+}
+
+getTime();
 
 exampleCall();
